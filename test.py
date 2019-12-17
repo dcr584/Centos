b53ef7ea0297f8051ba2fd07a308cfa53675473c
ho <ho@localhost.localdomain>
2019-12-18 00:50:40 +0900

    구매수량 입력되어있으면 더 이상 입력 안하게 해도됨

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
 import pymysql.cursors
 import sys
 
-input_ea=0;
+turn=0;
 
-#conn = pymysql.connect(host='localhost', user='root', password='123', charset='utf8mb4')
-sql1 = 'select * from study.information' #test1 출력
-sql2 = "insert into study.information(Date, Note, Count)values(%s, %s, %s)"#test1 입력
-sql3 = "insert into study.information(EA)values(%s)"  #티켓 수량 입력
-sql4 = 'select * from study.information where Ea OR NULL' #값이 있는 것 만 출력
-sql5 = "insert into study.information(Ea)values(%s)"#test1 입력
-
+sql1 = 'select * from Newbie.Content'
+sql2 = "insert into Newbie.Content(DATE, NAME, NOTE, C_NUM, P_TIME, USE_N)values(%s, %s, %s, %s, %s, %s)"
+sql3 = "insert into Newbie.Residue(RUSIDUE)values(%s)"
 def select():
-    conn = pymysql.connect(host='localhost', user='root', password='123', charset='utf8mb4')
+    conn = pymysql.connect(host='localhost', user='root', password='', charset='utf8mb4')
     try:
         with conn.cursor() as cursor:
-           # sql1 = 'select * from study_test.test1'
             cursor.execute(sql1)
             rows = cursor.fetchall()
             for i in rows:
@@ -23,52 +18,57 @@ def select():
     finally:
             conn.close()
 
-def insert(input_Date, input_Note, input_Count):
-    conn = pymysql.connect(host='localhost', user='root', password='123', charset='utf8mb4')
-
+def Content(input_date, input_name, input_note, input_c_num, input_p_time, input_use_n):
+    conn = pymysql.connect(host='localhost', user='root', password='', charset='utf8mb4') 
     try:
         with conn.cursor() as cursor:
-            #sql2 = "insert into study_test.test1(Date, Note, Count)values(%s, %s, %s)"
-            #if(input_ea <= 0):
-            cursor.execute(sql2, (input_Date, input_Note, input_Count))
-            #else:
-             #   input_ea = input_ea - input_Count
-             #   cursor.execute(sql2, (input_Date, input_Note, input_Count, input_ea))
-            #cursor.execute(sql5, (input_ea))
+            cursor.execute(sql2, (input_date, input_name, input_note, input_c_num, input_p_time, input_use_n))
             conn.commit()
     finally:
         conn.close()
 
-def EA(input_Ea):
-    conn = pymysql.connect(host='localhost', user='root', password='123', charset='utf8mb4')
-
+def Rusidue(input_rusidue):
+    conn = pymysql.connect(host='localhost', user='root', password='', charset='utf8mb4')
     try:
         with conn.cursor() as cursor:
-            #sql3 = "insert into study_test.EA_table(EA)values(%s)"
-            cursor.execute(sql3, (input_Ea)) 
+            cursor.execute(sql3, (input_rusidue)) 
             conn.commit()
     finally:
         conn.close()
 
 while True:
     print("1. 내용입력 2. 확인 3. 구매수량 4. 종료") #시작 메뉴
-    print("\n")
-    
+    print("\n") 
     num_menu = int(input("번호를 입력해 주세요 : "))
     print("\n")
 
     if(num_menu == 1):  #입력
-        input_date = str(input("날짜 : "))
-        input_note = str(input("내역 : "))
-        input_count = int(input("사용수량 : "))
-        insert(input_date, input_note, input_count)
+        print(turn)
+        if(turn <= 0):
+            print("구매수량을 먼저 입력해 주세요.")
+        else:
+            input_date=0
+            input_name=0
+            input_note=0
+            input_c_num=0
+            input_p_time=0
+            input_use_n=0
+
+            input_date = str(input("날짜 : "))
+            input_name = str(input("작성자 : "))
+            input_note = str(input("내역 : "))
+            input_c_num = str(input("차량번호 : "))
+            input_p_time = str(input("주차시간 : "))
+            input_use_n = int(input("사용수량 : "))
+            Content(input_date, input_name, input_note, input_c_num, input_p_time, input_use_n)
 
     elif(num_menu == 2): #확인
         select()
 
     elif(num_menu == 3): #구매수량
-        input_ea = int(input("구매수량 입력해 주세요 : "))
-        EA(input_ea)
+        turn+=1
+        input_rusidue = int(input("구매수량 입력해 주세요 : "))
+        Rusidue(input_rusidue)
 
     elif(num_menu == 4): #종료
         sys.exit()
